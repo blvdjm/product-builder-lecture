03384004b782126eb0c85fe7c6d9f24e9cd9e3c9
blvdjm <blvdjm@gmail.com>
2026-02-21 09:59:23 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+const numbersContainer = document.getElementById('numbers');
+const generateButton = document.getElementById('generate');
+
+generateButton.addEventListener('click', () => {
+    numbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    for (const number of [...numbers].sort((a, b) => a - b)) {
+        const numberElement = document.createElement('div');
+        numberElement.classList.add('number');
+        numberElement.textContent = number;
+        numbersContainer.appendChild(numberElement);
+    }
+});
